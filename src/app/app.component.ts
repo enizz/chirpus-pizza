0c49ce1e9465c04489c9c3f396aa567ec3ce1694
Eugene <Eugene.Nisnevich@Gmail.com>
2022-10-29 17:33:34 -0400

    added interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menuitem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chirpus-pizza';
+  menuItems: MenuItem[] = [
+    {
+    item: "Chicken Fingers",
+    category: "dinner",
+    price: 11.99
+    },
+    {
+      item: "Pizza",
+      category: "dinner",
+      price: 11.99
+    },
+    {
+      item: "Wings",
+      category: "side",
+      price: 8.99
+    },
+    {
+      item: "Breadsticks",
+      category: "side",
+      price: 4.99
+    },
+    {
+      item: "Casar Salad",
+      category: "salad",
+      price: 5.99
+    },
+    {
+      item: "Cinnamon Roll",
+      category: "dessert",
+      price: 8.99
+    }
+  ]
 }
